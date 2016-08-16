ee35263546a337002d6e5c72be419d57d64c2bf1
ztaira14 <ztaira14@punahou.edu>
2016-08-15 21:59:53 -0400

    Menu Update 8/15
    Menu works better, and has color.

--- main.cpp
+++ main.cpp
@@ -8,8 +8,10 @@ using namespace std;
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
-void open_screen(int y, int x);
+void open_screen();
 void reset_screen(int wheight, int wwidth);
+void draw_border(WINDOW *win);
+void configure_settings();
 
 string choices[] = {
     "Choice 1",
@@ -19,25 +21,13 @@ string choices[] = {
 
 int main()
 {
-    int ch, win_height, win_width;
+    int ch;
     // create a screen
     // get the screen's height and width
     initscr();
-    getmaxyx(stdscr, win_height, win_width);
-    // we don't want to print input characters
-    noecho();
-    // we want the cursor to be invisible
-    curs_set(0);
-    // make all keystrokes immediately available to the program
-    cbreak();
-    // we want to turn colors on
-    start_color();
-    init_pair(1, COLOR_WHITE, COLOR_BLACK);
-    init_pair(2, COLOR_BLACK, COLOR_WHITE);
-    init_pair(3, COLOR_GREEN, COLOR_BLACK);
-    wbkgd(stdscr, COLOR_PAIR(1));
+    configure_settings();
     // print the opening screen, which won't go away until a key is pressed
-    open_screen(win_height/2, win_width/2);
+    open_screen();
     refresh();
     getch();
     werase(stdscr);
@@ -58,10 +48,13 @@ int main()
     my_items[choicenum+1] = (ITEM *)NULL;
     // create the menu in a window
     my_menu = new_menu((ITEM **)my_items);
-    main_menu_win = newwin(40, 40, 0, 0);
+    main_menu_win = newwin(30, 30, 0, 0);
+    wattron(main_menu_win, COLOR_PAIR(8));
     keypad(main_menu_win, true);
+    set_menu_win(my_menu, main_menu_win);
+    set_menu_sub(my_menu, derwin(main_menu_win, 28, 28, 1, 1));
     post_menu(my_menu);
-    // cout << post_menu(my_menu);
+    draw_border(main_menu_win);
     refresh();
     while((ch = getch()) != 'q')
     {
@@ -69,11 +62,13 @@ int main()
         {
             case 'j':
                 menu_driver(my_menu, REQ_DOWN_ITEM);
-                refresh();
+                // refresh();
+                wrefresh(main_menu_win);
                 break;
             case 'k':
                 menu_driver(my_menu, REQ_UP_ITEM);
-                refresh();
+                // refresh();
+                wrefresh(main_menu_win);
                 break;
         }
     }   
@@ -88,8 +83,33 @@ int main()
     return 0;
 }
 
-void open_screen(int y, int x)
+void configure_settings()
 {
+    // we don't want to print input characters
+    noecho();
+    // we want the cursor to be invisible
+    curs_set(0);
+    // make all keystrokes immediately available to the program
+    cbreak();
+    // we want to turn colors on
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_BLACK, COLOR_WHITE);
+    init_pair(3, COLOR_RED, COLOR_BLACK);
+    init_pair(4, COLOR_GREEN, COLOR_BLACK);
+    init_pair(5, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(6, COLOR_BLUE, COLOR_BLACK);
+    init_pair(7, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(8, COLOR_CYAN, COLOR_BLACK);
+    wbkgd(stdscr, COLOR_PAIR(1));
+}
+
+void open_screen()
+{
+    int y, x;
+    getmaxyx(stdscr, y, x);
+    y = y/2;
+    x = x/2;
     string str;
     str = "Ukulele Tab Converter";
     mvprintw(y-2, x-str.length()/2, str.c_str());
@@ -100,13 +120,8 @@ void open_screen(int y, int x)
     mvprintw(y*2-1, 1, " ");
 }
 
-void reset_screen(int wheight, int wwidth)
+void draw_border(WINDOW *win)
 {
-    for (int i=0; i<wheight; ++i)
-    {
-        for (int j=0; j<wwidth; ++j)
-        {
-            mvprintw(i, j, " ");
-        }
-    }
+    wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
+    wrefresh(win);
 }
